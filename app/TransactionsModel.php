e3cc935fa2f0d3b7aab85560b583db63889c8d2c
Andreas-Steven <andreas15@gmail.com>
2020-06-02 08:33:04 +0700

    Small change on TransactionsModel

--- app/TransactionsModel.php
+++ app/TransactionsModel.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionsModel extends Model
 {
+    protected $primaryKey = 'ID';
     protected $table = 'transactions';
     protected $fillable = ['UserID', 'ProductID', 'PurchaseAmount'];
     protected $dates = ['deleted_at'];
